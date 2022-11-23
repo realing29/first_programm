a044ea9b7d35f9ccebe8ccbc8f93285281123ab4
Василаки Роман <roman.vasilaki@myway.local>
2022-11-23 11:19:24 +0500

    lesson 6 completed

--- notes.controller.js
+++ notes.controller.js
@@ -1,18 +1,25 @@
 const fs = require('fs/promises')
+const path = require('path')
+
+const notesPath = path.join(__dirname, 'db.json')
 
 const addNote = async (title) => {
-	const notes = require('./db.json')
+	const notes = await getNotes()
 
 	const note = {
 		title,
 		id: Date.now().toString(),
 	}
+	console.log('🚀 ~ file: notes.controller.js ~ line 10 ~ addNote ~ note', note)
 	notes.push(note)
-
-	await fs.writeFile('./db.json', JSON.stringify(notes))
+	await fs.writeFile(notesPath, JSON.stringify(notes))
 }
 
-const getNotes = () => require('./db.json')
+const getNotes = async () => {
+	let notes = await fs.readFile(notesPath, { encoding: 'utf-8' })
+	notes = JSON.parse(notes)
+	return Array.isArray(notes) ? notes : []
+}
 
 module.exports = {
 	addNote,
