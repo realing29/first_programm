58ad7f7c5443e388b8cbf5545075923ec247f1e7
realing29 <realing29@gmail.com>
2022-11-25 18:20:27 +0500

    started 4. web server and deleted example code

--- refs/path.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-console.log(__filename) // Обсолютный путь с файлом
-console.log(path.dirname(__filename)) // Обсолютный путь без файла
-console.log(path.basename(__filename)) // только имя файла
-console.log(path.extname(__filename)) // расширение с точкой || .slice(1) без точки
-console.log(path.parse(__filename)) // объект со всем предыдущим
-console.log(path.resolve(__dirname, '..', './modules', './app.js')) //
-console.log(path.join(__dirname, '..', './modules', './app.js')) //
