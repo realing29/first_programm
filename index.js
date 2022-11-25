974aaa6d5cc2b5fa88464abb352bfcfa3429eaad
realing29 <realing29@gmail.com>
2022-11-25 19:27:41 +0500

    completed 3. Processing request

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const chalk = require('chalk')
 const http = require('http')
 const fs = require('fs/promises')
 const path = require('path')
+const { addNote } = require('./notes.controller')
 
 const PORT = 3000
 
@@ -14,6 +15,20 @@ const server = http.createServer(async (req, res) => {
 		// res.setHeader('Content-Type', 'text/html')
 		res.writeHead(200, { 'Content-Type': 'text/html' })
 		res.end(content)
+	} else if (req.method === 'POST') {
+		const body = []
+
+		res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
+
+		req.on('data', (data) => {
+			body.push(Buffer.from(data))
+		})
+
+		req.on('end', () => {
+			const title = body.toString().split('=')[1].replaceAll('+', ' ')
+			addNote(title)
+			res.end(`Title = ${title}`)
+		})
 	}
 })
 
