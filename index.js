c50587516b716d69efb44bd619899a14dcbcf2f1
realing29 <realing29@gmail.com>
2022-11-25 19:07:00 +0500

    completed 3. output html

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const chalk = require('chalk')
 const http = require('http')
+const fs = require('fs/promises')
+const path = require('path')
 
 const PORT = 3000
 
-const server = http.createServer((req, res) => {
-	console.log('req', req)
-	res.end('Hello from server!!!')
+const basePath = path.join(__dirname, 'pages')
+
+const server = http.createServer(async (req, res) => {
+	if (req.method === 'GET') {
+		const content = await fs.readFile(path.join(basePath, 'index.html'))
+
+		// res.setHeader('Content-Type', 'text/html')
+		res.writeHead(200, { 'Content-Type': 'text/html' })
+		res.end(content)
+	}
 })
 
 server.listen(PORT, () => {
